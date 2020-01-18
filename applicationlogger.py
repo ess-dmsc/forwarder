2107605847465005221293c853bc4d836e94884b
Matthew D Jones <matthew.d.jones@tessella.com>
2020-01-18 21:37:20 +0000

    Add graylog logging

--- applicationlogger.py
+++ applicationlogger.py
@@ -1,12 +1,11 @@
 import logging
+import graypy
 
 
-def setup_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
+def setup_logger(name: str = "python-forwarder", level: int = logging.DEBUG) -> logging.Logger:
     logging.basicConfig()
     logger = logging.getLogger(name)
     logger.setLevel(level)
+    handler = graypy.GELFUDPHandler('localhost', 12201)
+    logger.addHandler(handler)
     return logger
-
-
-def get_logger() -> logging.Logger:
-    return logging.getLogger('python-forwarder')
