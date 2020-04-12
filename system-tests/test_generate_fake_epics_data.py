6702455ea5e612cedb08925a6c7843e40e53a2ec
Matthew D Jones <matthew.d.jones@tessella.com>
2020-04-12 11:10:01 +0100

    Remove things from system tests related to loading config from json

--- system-tests/test_generate_fake_epics_data.py
+++ system-tests/test_generate_fake_epics_data.py
@@ -2,8 +2,10 @@ from helpers.kafka_helpers import create_consumer, poll_for_valid_message
 from helpers.f142_logdata import Value
 from helpers.flatbuffer_helpers import check_expected_value
 from time import sleep
+import pytest
 
 
+@pytest.mark.skip(reason="Fake EPICS updates not implemented yet in python forwarder")
 def test_forwarder_sends_fake_pv_updates(docker_compose_fake_epics):
     # A fake PV is defined in the config json file with channel name "FakePV"
     consumer = create_consumer()
