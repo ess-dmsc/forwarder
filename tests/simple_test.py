1ba581919139a5b0a82f1f7e7968c53f71bbcb5f
Matthew D Jones <matthew.d.jones@tessella.com>
2020-04-13 15:57:49 +0100

    Start adding tools to test with PVA

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,8 +1,17 @@
-# MANUAL TEST
+# MANUAL TEST CA
 # docker-compose up (start a local kafka cluster)
 # Start incrementing_ioc.py
 # Start forwarder.py
 # Send config message to add PV: kafkacat -P -b localhost -t forwarder-config config_message_add.json
 # Wait
+# Send config message to remove PV: kafkacat -P -b localhost -t forwarder-config config_message_remove.json
+# Stop everything
+
+# MANUAL TEST PVA
+# docker-compose up (start a local kafka cluster)
+# Start pva_server.py
+# Start forwarder.py
+# Send config message to add PV: kafkacat -P -b localhost -t forwarder-config config_message_add.json
+# Update PV values using pva_client.py
 # Send config message to remove PV: kafkacat -P -b localhost -t forwarder-config config_message_remove.json
 # Stop everything
