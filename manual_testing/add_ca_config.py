27d71778d62037a24417c8c607ac9917dc2c81f2
Matthew D Jones <matthew.d.jones@tessella.com>
2020-05-25 12:22:44 +0100

    Add manual test scripts to add CA and PVA pv config

---
+++ manual_testing/add_ca_config.py
@@ -0,0 +1,29 @@
+from forwarder.kafka.kafka_helpers import create_producer
+
+"""
+"docker-compose up" first!
+"""
+
+
+if __name__ == "__main__":
+    config_message = (
+        "{"
+        '  "cmd": "add",'
+        '  "streams": ['
+        "    {"
+        '      "channel": "SIMPLE:DOUBLE3",'
+        '      "channel_provider_type": "ca",'
+        '      "converter": {'
+        '        "schema": "f142",'
+        '        "topic": "localhost:9092/ca_data"'
+        "      }"
+        "    }"
+        "  ]"
+        "}"
+    )
+
+    print(config_message)
+
+    producer = create_producer("localhost:9092")
+    producer.produce("forwarder_config", config_message.encode("utf8"))
+    producer.close()
