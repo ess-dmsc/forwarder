63bad0a2346e92941a6d6b66bda5490e60679c83
Matthew D Jones <matthew.d.jones@tessella.com>
2020-06-19 13:27:11 +0100

    #25 Update StatusReporter tests to expect flatbuffer payload

--- tests/status_reporter_test.py
+++ tests/status_reporter_test.py
@@ -1,6 +1,7 @@
 from forwarder.status_reporter import StatusReporter
 from typing import Optional, Dict
 import json
+from streaming_data_types.status_x5f2 import deserialise_x5f2
 
 
 class FakeProducer:
@@ -33,7 +34,8 @@ def test_when_update_handlers_exist_their_channel_names_are_reported_in_status()
     status_reporter.report_status()
 
     if fake_producer.published_payload is not None:
-        produced_status_message = json.loads(fake_producer.published_payload)
+        deserialised_payload = deserialise_x5f2(fake_producer.published_payload)
+        produced_status_message = json.loads(deserialised_payload.status_json)
     # Using set comprehension as order is unimportant
     assert {
         stream["channel_name"] for stream in produced_status_message["streams"]
@@ -51,7 +53,21 @@ def test_when_no_update_handlers_exist_no_streams_are_present_in_reported_status
     status_reporter.report_status()
 
     if fake_producer.published_payload is not None:
-        produced_status_message = json.loads(fake_producer.published_payload)
+        deserialised_payload = deserialise_x5f2(fake_producer.published_payload)
+        produced_status_message = json.loads(deserialised_payload.status_json)
     assert (
         len(produced_status_message["streams"]) == 0
     ), "Expected no streams in reported status message as there are no update handlers"
+
+
+def test_status_message_contains_service_id():
+    service_id = "test_service_id"
+    update_handlers: Dict = {}
+
+    fake_producer = FakeProducer()
+    status_reporter = StatusReporter(update_handlers, fake_producer, "status_topic", service_id)  # type: ignore
+    status_reporter.report_status()
+
+    if fake_producer.published_payload is not None:
+        deserialised_payload = deserialise_x5f2(fake_producer.published_payload)
+    assert deserialised_payload.service_id == service_id
