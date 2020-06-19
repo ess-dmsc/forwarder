d3d769710435a57ca03ecbe7b6b0a85626946f76
Matthew D Jones <matthew.d.jones@tessella.com>
2020-06-19 13:37:21 +0100

    #25 Implement using flatbuffer for status messages

--- forwarder/status_reporter.py
+++ forwarder/status_reporter.py
@@ -1,8 +1,11 @@
 from forwarder.repeat_timer import RepeatTimer, milliseconds_to_seconds
 from forwarder.kafka.kafka_producer import KafkaProducer
 from typing import Dict
+from streaming_data_types.status_x5f2 import serialise_x5f2
 import json
 import time
+from socket import gethostname
+from os import getpid
 
 
 class StatusReporter:
@@ -21,19 +24,29 @@ class StatusReporter:
         self._topic = topic
         self._update_handlers = update_handlers
         self._service_id = service_id
+        self._interval_ms = interval_ms
 
     def start(self):
         self._repeating_timer.start()
 
     def report_status(self):
-        status_message = json.dumps(
+        status_json = json.dumps(
             {
                 "streams": [
                     {"channel_name": channel_name}
                     for channel_name in self._update_handlers.keys()
                 ]
             }
-        ).encode("utf-8")
+        )
+        status_message = serialise_x5f2(
+            "Forwarder",
+            "version",
+            self._service_id,
+            gethostname(),
+            getpid(),
+            self._interval_ms,
+            status_json,
+        )
         self._producer.produce(self._topic, status_message, int(time.time() * 1000))
 
     def stop(self):
