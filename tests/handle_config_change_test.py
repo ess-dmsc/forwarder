524aca069d32469e7c6c3f452320ddafe560ff40
Matthew D Jones <matthew.d.jones@tessella.com>
2020-07-21 22:47:23 +0100

    #39 Unit test to cover adding identical channel configurations

--- tests/handle_config_change_test.py
+++ tests/handle_config_change_test.py
@@ -119,6 +119,40 @@ def test_update_handlers_are_added_when_add_config_update_is_handled(update_hand
     assert channel_name_1 in _get_channel_names(update_handlers)
     assert channel_name_2 in _get_channel_names(update_handlers)
 
-    # Clean up
-    for _, handler in update_handlers.items():
-        handler.stop()
+
+def test_can_add_multiple_channels_with_same_name_if_protocol_topic_or_schema_are_different(
+    update_handlers,
+):
+    status_reporter = StubStatusReporter()
+    producer = FakeProducer()
+    channel_name = "test_channel"
+    test_channel_1 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic", "f142")
+    test_channel_2 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic_2", "f142")
+    test_channel_3 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic", "tdct")
+    config_update = ConfigUpdate(
+        CommandType.ADD, (test_channel_1, test_channel_2, test_channel_3,),
+    )
+
+    handle_configuration_change(config_update, 20000, None, update_handlers, producer, None, None, _logger, status_reporter)  # type: ignore
+    assert len(update_handlers) == 3
+    assert test_channel_1 in update_handlers.keys()
+    assert test_channel_2 in update_handlers.keys()
+    assert test_channel_3 in update_handlers.keys()
+
+
+def test_identical_configurations_are_not_added(update_handlers,):
+    status_reporter = StubStatusReporter()
+    producer = FakeProducer()
+    channel_name = "test_channel"
+    test_channel_1 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic", "f142")
+    test_channel_2 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic", "f142")
+    test_channel_3 = Channel(channel_name, EpicsProtocol.FAKE, "output_topic", "f142")
+    config_update = ConfigUpdate(
+        CommandType.ADD, (test_channel_1, test_channel_2, test_channel_3,),
+    )
+
+    handle_configuration_change(config_update, 20000, None, update_handlers, producer, None, None, _logger, status_reporter)  # type: ignore
+    assert (
+        len(update_handlers) == 1
+    ), "Only expect one channel to be added as others requested were identical"
+    assert test_channel_1 in update_handlers.keys()
