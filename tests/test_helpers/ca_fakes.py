80c6606aded6759d95f323a8e6907c76027477b9
Matthew D Jones <matthew.d.jones@tessella.com>
2020-07-25 16:42:29 +0100

    re #41 Add test for handling ca float updates

--- tests/test_helpers/ca_fakes.py
+++ tests/test_helpers/ca_fakes.py
@@ -1,19 +1,39 @@
 from typing import List, Callable
+from caproto import ReadNotifyResponse
 
 
 class FakeSubscription:
-    @staticmethod
-    def add_callback(callback: Callable):
-        pass
+    def __init__(self):
+        def callback_undefined(*args, **kwargs):
+            raise Exception(
+                "Callback was called before it was set in FakeSubscription object"
+            )
+
+        self.callback: Callable = callback_undefined
+
+    def add_callback(self, callback: Callable):
+        self.callback = callback
 
 
 class FakePV:
+    def __init__(self, pv_name: str, subscription: FakeSubscription):
+        self.name = pv_name
+        self.subscription = subscription
+
+    def subscribe(self, data_type: str) -> FakeSubscription:
+        return self.subscription
+
     @staticmethod
-    def subscribe(data_type: str) -> FakeSubscription:
-        return FakeSubscription()
+    def unsubscribe_all():
+        pass
 
 
 class FakeContext:
-    @staticmethod
-    def get_pvs(*pv_names: str) -> List[FakePV]:
-        return [FakePV() for _ in pv_names]
+    def __init__(self):
+        self.subscription = FakeSubscription()
+
+    def get_pvs(self, *pv_names: str) -> List[FakePV]:
+        return [FakePV(pv_name, self.subscription) for pv_name in pv_names]
+
+    def call_monitor_callback_with_fake_pv_update(self, pv_update: ReadNotifyResponse):
+        self.subscription.callback(self.subscription, pv_update)
--- tests/update_handlers/ca_update_handler_test.py
+++ tests/update_handlers/ca_update_handler_test.py
@@ -1,7 +1,11 @@
 from tests.kafka.fake_producer import FakeProducer
 from forwarder.update_handlers.ca_update_handler import CAUpdateHandler
 from tests.test_helpers.ca_fakes import FakeContext
+from cmath import isclose
+from streaming_data_types.logdata_f142 import deserialise_f142
 import pytest
+from caproto import ReadNotifyResponse, ChannelType, TimeStamp
+import numpy as np
 
 
 def test_update_handler_throws_if_schema_not_recognised():
@@ -10,3 +14,39 @@ def test_update_handler_throws_if_schema_not_recognised():
     non_existing_schema = "DOESNTEXIST"
     with pytest.raises(ValueError):
         CAUpdateHandler(producer, context, "source_name", "output_topic", non_existing_schema)  # type: ignore
+
+
+@pytest.mark.parametrize(
+    "pv_value,pv_caproto_type,pv_numpy_type",
+    [
+        (4.2222, ChannelType.TIME_DOUBLE, np.float64),
+        (4.2, ChannelType.TIME_FLOAT, np.float32),
+    ],
+)
+def test_update_handler_publishes_float_update(
+    pv_value, pv_caproto_type, pv_numpy_type
+):
+    producer = FakeProducer()
+    context = FakeContext()
+
+    pv_source_name = "source_name"
+    update_handler = CAUpdateHandler(producer, context, pv_source_name, "output_topic", "f142")  # type: ignore
+
+    metadata = (0, 0, TimeStamp(4, 0))
+    context.call_monitor_callback_with_fake_pv_update(
+        ReadNotifyResponse(
+            np.array([pv_value]).astype(pv_numpy_type),
+            pv_caproto_type,
+            1,
+            1,
+            1,
+            metadata=metadata,
+        )
+    )
+
+    assert producer.published_payload is not None
+    pv_update_output = deserialise_f142(producer.published_payload)
+    assert isclose(pv_update_output.value, pv_value, abs_tol=0.0001)
+    assert pv_update_output.source_name == pv_source_name
+
+    update_handler.stop()
