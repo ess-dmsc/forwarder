f655afc496f47ca29d3889b0f3fb5bbd32acf9ea
Matthew D Jones <matthew.d.jones@tessella.com>
2020-08-10 14:42:31 +0100

    Add test for new chopper timestamp scheme

--- tests/kafka/kafka_helpers_test.py
+++ tests/kafka/kafka_helpers_test.py
@@ -1,4 +1,8 @@
 from forwarder.kafka.kafka_helpers import get_broker_and_topic_from_uri
+from forwarder.kafka.kafka_helpers import publish_tdct_message
+from tests.kafka.fake_producer import FakeProducer
+from streaming_data_types.timestamps_tdct import deserialise_tdct
+import numpy as np
 import pytest
 
 
@@ -33,3 +37,22 @@ def test_uri_with_port_after_broker_is_included_in_broker_output():
     broker, topic = get_broker_and_topic_from_uri(test_uri)
     assert broker == test_broker
     assert topic == test_topic
+
+
+def test_tdct_publisher_converts_relative_timestamp_converted_to_absolute():
+    producer = FakeProducer()
+    input_relative_timestamps = np.array([1, 2, 3]).astype(np.uint32)
+    reference_timestamp = 10
+    publish_tdct_message(
+        producer,  # type: ignore
+        "test_topic",
+        input_relative_timestamps,
+        "test_source",
+        reference_timestamp,
+    )
+
+    assert producer.published_payload is not None
+    published_data = deserialise_tdct(producer.published_payload)
+    assert np.array_equal(
+        published_data.timestamps, input_relative_timestamps + reference_timestamp
+    )
