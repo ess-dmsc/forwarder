8cf4a7e040b71071b5b5bfec48e66f9f22f18ca3
Matthew D Jones <matthew.d.jones@tessella.com>
2020-08-15 14:19:48 +0100

    re #3 Update manual config add scripts

--- manual_testing/add_fake_config.py
+++ manual_testing/add_fake_config.py
@@ -1,27 +1,27 @@
 from forwarder.kafka.kafka_helpers import create_producer
+from streaming_data_types.forwarder_config_update_rf5k import serialise_rf5k, StreamInfo
+from streaming_data_types.fbschemas.forwarder_config_update_rf5k.UpdateType import (
+    UpdateType,
+)
+from streaming_data_types.fbschemas.forwarder_config_update_rf5k.Protocol import (
+    Protocol,
+)
 import time
 
+"""
+"docker-compose up" first!
+"""
+
+
 if __name__ == "__main__":
-    config_message = (
-        "{"
-        '  "cmd": "add",'
-        '  "streams": ['
-        "    {"
-        '      "channel": "fake:int",'
-        '      "channel_provider_type": "fake",'
-        '      "converter": {'
-        '        "schema": "f142",'
-        '        "topic": "localhost:9092/fake_data"'
-        "      }"
-        "    }"
-        "  ]"
-        "}"
-    )
-
-    print(config_message)
-
     producer = create_producer("localhost:9092")
+
     producer.produce(
-        "forwarder_config", config_message.encode("utf8"), int(time.time() * 1000)
+        "forwarder_config",
+        serialise_rf5k(
+            UpdateType.ADD,
+            [StreamInfo("fake:int", "f142", "fake_data", Protocol.FAKE,)],
+        ),
+        int(time.time() * 1000),
     )
     producer.close()
