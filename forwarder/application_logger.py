c2393eee52f80bc560a4f6565d1180d70145a627
Ebad Kamil <ebad.kamil@ess.eu>
2021-01-25 09:14:03 +0100

    Create hidden logger folder in HOME directory

--- forwarder/application_logger.py
+++ forwarder/application_logger.py
@@ -1,8 +1,14 @@
+import os
 import logging
 import graypy
 from typing import Optional
 
+
 logger_name = "python-forwarder"
+
+ROOT_PATH = os.path.join(os.path.expanduser("~"), ".python-forwarder")
+if not os.path.isdir(ROOT_PATH):
+    os.makedirs(ROOT_PATH)
 
 
 def setup_logger(
@@ -11,7 +17,9 @@ def setup_logger(
     graylog_logger_address: Optional[str] = None,
 ) -> logging.Logger:
     if log_file_name is not None:
-        logging.basicConfig(filename=log_file_name, format="%(asctime)s %(message)s")
+        logging.basicConfig(
+            filename=os.path.join(ROOT_PATH, log_file_name),
+            format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
     else:
         logging.basicConfig()
     logger = logging.getLogger(logger_name)
