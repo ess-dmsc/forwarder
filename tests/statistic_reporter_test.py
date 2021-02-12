6ca03d1329b57d2160a0d23dfb19b1e447390efc
Ebad Kamil <ebad.kamil@ess.eu>
2021-02-12 12:22:06 +0100

    Unittest: Update the StatisticReporter tests

--- tests/statistic_reporter_test.py
+++ tests/statistic_reporter_test.py
@@ -1,6 +1,7 @@
 import logging
-import time
-from unittest.mock import MagicMock
+import queue
+from typing import Dict
+from unittest.mock import MagicMock, patch
 
 from forwarder.statistics_reporter import StatisticsReporter
 
@@ -10,31 +11,30 @@ logger.addHandler(logging.NullHandler())
 
 
 def test_that_warning_logged_on_send_exception(caplog):
-    statistics_reporter = StatisticsReporter("localhost", logger)
+    update_handler: Dict = {}
+    statistics_reporter = StatisticsReporter(
+        "localhost", update_handler, queue.Queue(), logger
+    )
     statistics_reporter._sender = MagicMock()
     statistics_reporter._sender.send.side_effect = ValueError
+
     with caplog.at_level(logging.WARNING):
-        statistics_reporter.send_pv_numbers("NOTINTORFLOAT", time.time())
+        statistics_reporter.send_statistics()
     assert "Could not send statistic: " in caplog.text
 
 
-def test_that_send_called_only_after_update_intervals():
-    statistics_reporter = StatisticsReporter("localhost", logger)
+@patch("forwarder.statistics_reporter.time")
+def test_that_send_statistics_sends_correct_message(mock_time):
+    timestamp = 100000
+    mock_time.time.return_value = timestamp
+
+    update_handler: Dict = {}
+    statistics_reporter = StatisticsReporter(
+        "localhost", update_handler, queue.Queue(), logger
+    )
     statistics_reporter._sender = MagicMock()
 
-    # test that first message is sent
-    first_msg_time = int(time.time())
-    statistics_reporter.send_pv_numbers(2, first_msg_time)
-    assert first_msg_time == statistics_reporter._last_update_s
-
-    # Test that message is not sent before update_interval time is complete
-    second_msg_time = first_msg_time + statistics_reporter._update_interval_s // 2
-    statistics_reporter.send_pv_numbers(2, second_msg_time)
-    statistics_reporter._sender.send.assert_called_once()
-    assert second_msg_time != statistics_reporter._last_update_s
-
-    # Test that message gets sent after update_interval time is complete
-    third_msg_time = second_msg_time + statistics_reporter._update_interval_s // 2 + 2
-    statistics_reporter.send_pv_numbers(2, third_msg_time)
-    assert statistics_reporter._sender.send.call_count == 2
-    assert third_msg_time == statistics_reporter._last_update_s
+    statistics_reporter.send_statistics()
+    statistics_reporter._sender.send.assert_called_once_with(
+        "number_pvs", len(update_handler.keys()), timestamp
+    )
