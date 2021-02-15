7ee73e048ab47eff4ac3cbb913e6d95a0310a7f1
Ebad Kamil <ebad.kamil@ess.eu>
2021-02-15 09:45:09 +0100

    Add andgry type annotation for list

--- forwarder/statistics_reporter.py
+++ forwarder/statistics_reporter.py
@@ -1,7 +1,8 @@
+from collections import Counter
 from logging import Logger
-from queue import Queue
+from queue import Queue, Empty
 import time
-from typing import Dict
+from typing import Dict, List
 
 import graphyte  # type: ignore
 
@@ -15,16 +16,17 @@ class StatisticsReporter:
         self,
         graphyte_server: str,
         update_handlers: Dict[Channel, UpdateHandler],
-        update_message_queue: Queue,
+        update_msg_queue: Queue,
         logger: Logger,
         prefix: str = "throughput",
-        update_interval_s: int = 10,
+        update_interval_s: int = 1,
     ):
         self._graphyte_server = graphyte_server
         self._update_handlers = update_handlers
-        self._update_message_queue = update_message_queue
+        self._update_msg_queue = update_msg_queue
         self._logger = logger
 
+        self._total_updates: List[str] = []
         self._sender = graphyte.Sender(self._graphyte_server, prefix=prefix)
         self._repeating_timer = RepeatTimer(update_interval_s, self.send_statistics)
 
@@ -32,12 +34,25 @@ class StatisticsReporter:
         self._repeating_timer.start()
 
     def send_statistics(self):
+        # Clear the _update_message_queue
+        while not self._update_msg_queue.empty():
+            try:
+                self._total_updates.append(self._update_msg_queue.get_nowait())
+            except Empty:
+                continue
+            self._update_msg_queue.task_done()
+
         timestamp = time.time()
         number_pvs = len(self._update_handlers.keys())
         try:
             self._sender.send("number_pvs", number_pvs, timestamp)
+            for channel, counts in Counter(self._total_updates).items():
+                print(channel, counts)
+                self._sender.send(channel, counts, timestamp)
+
         except Exception as ex:
             self._logger.error(f"Could not send statistic: {ex}")
 
     def stop(self):
+        self._total_updates.clear()
         self._repeating_timer.cancel()
