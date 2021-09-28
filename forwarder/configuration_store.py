e469f39d5ae356ecfa3a53e9de902e807a890a83
Ebad Kamil <ebad.kamil@ess.eu>
2021-09-28 11:34:24 +0200

    Try to retrieve a valid config buffer from storage_topic

--- forwarder/configuration_store.py
+++ forwarder/configuration_store.py
@@ -3,8 +3,6 @@ from typing import Dict
 from unittest import mock
 
 from confluent_kafka import TopicPartition
-from flatbuffers.packer import struct as flatbuffer_struct
-from streaming_data_types.exceptions import WrongSchemaException
 from streaming_data_types.fbschemas.forwarder_config_update_rf5k.UpdateType import (
     UpdateType,
 )
@@ -16,15 +14,6 @@ from streaming_data_types.forwarder_config_update_rf5k import (
 )
 
 from forwarder.parse_config_update import EpicsProtocol
-
-
-def is_a_valid_configuration(payload):
-    validated = True
-    try:
-        _ = deserialise_rf5k(payload)
-    except (RuntimeError, flatbuffer_struct.error, WrongSchemaException):
-        validated = False
-    return validated
 
 
 class ConfigurationStore:
@@ -57,22 +46,42 @@ class ConfigurationStore:
         self._producer.produce(self._topic, bytes(message), int(time.time() * 1000))
 
     def retrieve_configuration(self):
-        # Retrieve last message
+        # Retrieve last valid configuration buffer from partition 0
         topic = TopicPartition(self._topic, 0)
-        _, high_offset = self._consumer.get_watermark_offsets(topic)
+        low_offset, high_offset = self._consumer.get_watermark_offsets(topic)
+        # Set offset to high_offset - 1 to start retrieving from last message
         topic.offset = high_offset - 1
         self._consumer.assign([topic])
 
-        msg = self._consumer.consume(timeout=2)
+        stored_config_buffer = None
 
-        if msg:
-            return msg[~0].value()
+        while high_offset >= low_offset:
+            msg = self._consumer.consume(timeout=2)
+            if msg:
+                payload = msg[-1].value()
+                if self._is_a_valid_configuration_buffer(payload):
+                    stored_config_buffer = payload
+                    break
+            high_offset -= 1
+            self._consumer.seek(TopicPartition(self._topic, 0, high_offset))
+
+        if stored_config_buffer:
+            return stored_config_buffer
         else:
             raise RuntimeError("Could not retrieve stored configuration")
 
     def stop(self):
         self._producer.close()
         self._consumer.close()
+
+    @staticmethod
+    def _is_a_valid_configuration_buffer(payload):
+        validated = True
+        try:
+            _ = deserialise_rf5k(payload)
+        except Exception:
+            validated = False
+        return validated
 
 
 NullConfigurationStore = mock.create_autospec(ConfigurationStore)
