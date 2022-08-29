4987bbbaf22f0d0234ae1f9d58d5ddb651b17033
Daniel Cacabelos <daniel.cacabelos@ess.eu>
2022-08-29 23:27:31 +0200

    Add support for Kafka SASL SCRAM authentication
    Only SCRAM-SHA-256 mechanism over plaintext (non-TLS) is supported.
    Integration tests are not updated.

--- forwarder/kafka/kafka_helpers.py
+++ forwarder/kafka/kafka_helpers.py
@@ -12,8 +12,29 @@ from forwarder.utils import Counter
 from .kafka_producer import KafkaProducer
 
 
+def sasl_config(username: Optional[str] = None, password: Optional[str] = None) -> dict:
+    """Return a dict with SASL SCRAM-SHA-256 configuration parameters."""
+    if not username or not password:
+        return {}
+    sasl_config = {
+        # Supported mechanisms: GSSAPI, PLAIN, SCRAM-SHA-512, SCRAM-SHA-256
+        "sasl.mechanism": "SCRAM-SHA-256",
+        # SASL_PLAINTEXT for plaintext, SASL_SSL for encrypted
+        "security.protocol": "SASL_PLAINTEXT",
+    }
+    sasl_config.update(
+        {
+            "sasl.username": username,
+            "sasl.password": password,
+        }
+    )
+    return sasl_config
+
+
 def create_producer(
     broker_address: str,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
     counter: Optional[Counter] = None,
     buffer_err_counter: Optional[Counter] = None,
 ) -> KafkaProducer:
@@ -21,6 +42,7 @@ def create_producer(
         "bootstrap.servers": broker_address,
         "message.max.bytes": "20000000",
     }
+    producer_config.update(sasl_config(username, password))
     producer = Producer(producer_config)
     return KafkaProducer(
         producer,
@@ -29,25 +51,40 @@ def create_producer(
     )
 
 
-def create_consumer(broker_address: str) -> Consumer:
-    return Consumer(
-        {
-            "bootstrap.servers": broker_address,
-            "group.id": uuid.uuid4(),
-            "default.topic.config": {"auto.offset.reset": "latest"},
-        }
-    )
+def create_consumer(
+    broker_address: str,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+) -> Consumer:
+    consumer_config = {
+        "bootstrap.servers": broker_address,
+        "group.id": uuid.uuid4(),
+        "default.topic.config": {"auto.offset.reset": "latest"},
+    }
+    consumer_config.update(sasl_config(username, password))
+    return Consumer(consumer_config)
 
 
-def get_broker_and_topic_from_uri(uri: str) -> Tuple[str, str]:
+def get_broker_topic_and_username_from_uri(uri: str) -> Tuple[str, str, str]:
     if "/" not in uri:
         raise RuntimeError(
-            f"Unable to parse URI {uri}, should be of form localhost:9092/topic"
+            f"Unable to parse URI {uri}, should be of form [username@]localhost:9092/topic"
         )
     topic = uri.split("/")[-1]
-    broker = "".join(uri.split("/")[:-1])
+    broker_and_username = "".join(uri.split("/")[:-1])
+    broker, username = get_broker_and_username_from_uri(broker_and_username)
+    return broker, topic, username
+
+
+def get_broker_and_username_from_uri(uri: str) -> Tuple[str, str]:
+    if "/" in uri:
+        raise RuntimeError(
+            f"Unable to parse URI {uri}, should be of form [username@]localhost:9092"
+        )
+    username = "".join(uri.split("@")[:-1])
+    broker = uri.split("@")[-1]
     broker = broker.strip("/")
-    return broker, topic
+    return broker, username
 
 
 def _nanoseconds_to_milliseconds(time_ns: int) -> int:
