630e2b2c050aa6718d48fb94394d9425ce808768
Matt Clarke <matt.clarke@esss.se>
2022-12-19 15:29:13 +0100

    try to make mypy happy

--- tests/update_handlers/nttable_senv_test.py
+++ tests/update_handlers/nttable_senv_test.py
@@ -11,6 +11,7 @@ from forwarder.update_handlers.nttable_senv_serialiser import nttable_senv_PVASe
 def test_serialise_nttable():
     values = np.arange(-50, 50, dtype=np.int16)
     timestamps = np.arange(50, 150, dtype=np.uint64)
+    first_timestamp = timestamps[0] / 1e9
 
     table = NTTable.buildType(
         columns=[
@@ -38,6 +39,6 @@ def test_serialise_nttable():
     assert fb_update.values.dtype == values.dtype
     assert np.array_equal(fb_update.value_ts, timestamps)
     assert fb_update.timestamp == datetime.fromtimestamp(
-        timestamps[0] / 1e9, tz=timezone.utc
+        first_timestamp, tz=timezone.utc
     )
     assert fb_update.message_counter == 0
