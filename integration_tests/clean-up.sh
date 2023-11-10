7c380fa18a41cc28e50ea921dfe2018d8eb46415
Afonso Mukai <afonso.mukai@ess.eu>
2023-11-10 13:41:48 +0100

    Remove python venv from integration tests

--- integration_tests/clean-up.sh
+++ integration_tests/clean-up.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 echo "Stopping and destroying containers..."
-source test_env/bin/activate
 docker compose down
 
 echo "Removing directories..."
-rm -rf test_env || true
 rm -rf shared_source/* || true
 
 echo "Clean-up completed!"
--- integration_tests/run-contract-tests.sh
+++ integration_tests/run-contract-tests.sh
@@ -10,7 +10,7 @@ echo "Sleeping..."
 sleep 10
 echo "Continuing!"
 
-rsync -av .. shared_volume/forwarder --exclude=integration_test/shared_volume --exclude=".*" --exclude=integration_test/test_env --exclude=venv
+rsync -av .. shared_volume/forwarder --exclude=integration_test/shared_volume --exclude=".*" --exclude=venv
 
 docker exec integration_tests-forwarder-1 bash -c 'cd /shared_source/forwarder/; scl enable rh-python38 -- python -m pip install --user --proxy "$HTTPS_PROXY" -r requirements.txt'
 docker exec integration_tests-forwarder-1 bash -c 'cd /shared_source/forwarder/; scl enable rh-python38 -- python -m pip install --user --proxy "$HTTPS_PROXY" pytest'
