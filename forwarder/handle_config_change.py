51cb893238c6a0993b710c3a751936a59c1a972a
Jonas Petersson <jonas.petersson@ess.eu>
2024-05-27 16:42:23 +0200

    starting to add configuration of periodicity

--- forwarder/handle_config_change.py
+++ forwarder/handle_config_change.py
@@ -44,7 +44,7 @@ def _subscribe_to_pv(
             pva_ctx,
             new_channel,
             fake_pv_period,
-            periodic_update_ms=pv_update_period,
+            periodic_update_ms=pv_update_period if new_channel.periodic else None,
             statistics_reporter=statistics_reporter,
             processing_errors_metric=processing_errors_metric,
         )
