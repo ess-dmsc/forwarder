217be767bc8de5d361eb75c47d5528b94620ff4e
Matt Clarke <matt.clarke@ess.eu>
2024-09-11 13:06:58 +0200

    remove graylog and use syslog instead

--- forwarder/application_logger.py
+++ forwarder/application_logger.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Optional
 
-import graypy
 
 logger_name = "python-forwarder"
 
@@ -14,16 +13,12 @@ def setup_logger(
     if log_file_name is not None:
         logging.basicConfig(
             filename=log_file_name,
-            format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s",
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         )
     else:
         logging.basicConfig()
     logger = logging.getLogger(logger_name)
     logger.setLevel(level)
-    if graylog_logger_address is not None:
-        host, port = graylog_logger_address.split(":")
-        handler = graypy.GELFTCPHandler(host, int(port), facility="ESS")
-        logger.addHandler(handler)
 
 
 def get_logger():
