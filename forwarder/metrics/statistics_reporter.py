5c0e39e2f606705f16fddc83b10e056e895b88fc
André Costa <andre.costa@ess.eu>
2026-07-15 20:30:51 +0200

    Add heartbeat metric to StatisticsReporter

--- forwarder/metrics/statistics_reporter.py
+++ forwarder/metrics/statistics_reporter.py
@@ -38,8 +38,9 @@ class StatisticsReporter:
 
     def send_statistics(self):
         try:
+            timestamp = time.time()
+            self._sender.send("metrics_registered", len(self._metrics), timestamp)
             for metric_name, metric in self._metrics.items():
-                timestamp = time.time()
                 if isinstance(metric, (Counter, Gauge)):
                     self._sender.send(metric_name, metric.value, timestamp)
                 elif isinstance(metric, Summary):
